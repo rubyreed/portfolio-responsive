65ace4acd626934a79e82df9ace9d171ceeccd33
Ruby Reed <rubysreed@gmail.com>
2022-02-09 23:29:56 -0700

    profile pic added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Header } from "../components/Styles";
+import ruby from "../images/ruby.jpg"
 
 const About = () => {
     return (
-        <Container>
-            <h1>About</h1>
+        <Container style={{display:"flex", justifyContent:"center"}}>
+            <img style={{height:"25rem", width:"25rem", objectFit:"cover", borderRadius:"12.5rem", marginTop:"5rem", boxShadow: "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset"}} src={ruby} alt="ruby"/>
         </Container>
     )
 }
