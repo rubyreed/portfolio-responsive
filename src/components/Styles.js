f3950f30ee21bcff38dd77a13717c016d648adc1
Ruby Reed <rubysreed@gmail.com>
2022-02-20 22:32:02 -0700

    about styling

--- src/components/Styles.js
+++ src/components/Styles.js
@@ -24,7 +24,6 @@ border-radius: .3rem;
 box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
 outline: none;
 border-style: none;
-font-size: .7rem;
 width: 5rem;
 &:hover {
   background-color: #BFBFBF;
--- src/pages/About.js
+++ src/pages/About.js
@@ -19,7 +19,7 @@ const About = () => {
         <div>
         <Container style={{display:"flex", justifyContent:"center", height: "100%"}}>
             <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginRight: "4vw", maxWidth:"60vw", marginBottom:"5vw"}}>
-                <img style={{height:"25vw", width:"25vw", objectFit:"cover", borderRadius:"12.5vw", marginTop:"5vw", marginBottom:"1vw", boxShadow: "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset"}} src={ruby} alt="ruby"/>
+                <img className="profileimg" style={{objectFit:"cover", marginTop:"5vw", marginBottom:"1vw", boxShadow: "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset"}} src={ruby} alt="ruby"/>
                 <div style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>
                 <a href = "https://github.com/rubyreed" target="_blank"><img style={{minHeight: "1.5rem", height:"3vw", padding:"1vw"}} src={githubwhite}/></a>
                 <a href = "https://www.linkedin.com/in/ruby-s-reed/" target="_blank"><img style={{minHeight:"1.5rem", height:"3vw", padding:"1vw"}} src={linkedinwhite}/></a>
@@ -36,7 +36,7 @@ const About = () => {
                 from the University of Utah in 2022 and have been learning and building since then.
                 </center></p>
                 <form method="get" action="resume.pdf" target="_blank" download>
-                <LightButton style={{width:"15vw", marginTop:"5vw", padding:"1vw"}}>View My Resume</LightButton>
+                <LightButton className="lightbutton" style={{width:"15vw", marginTop:"5vw", padding:"1vw"}}>View My Resume</LightButton>
                 </form>
             </div>
         </Container>
